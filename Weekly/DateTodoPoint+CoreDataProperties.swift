1e8496eeadea8ec2e02a88c04c48c87ee735780c
Sungyong Yun <yo0230@gmail.com>
2015-08-01 18:08:22 +0900

    NSManagedObject 생성

---
+++ Weekly/DateTodoPoint+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+//
+//  DateTodoPoint+CoreDataProperties.swift
+//  
+//
+//  Created by YunSeungyong on 2015. 8. 1..
+//
+//
+//  Delete this file and regenerate it using "Create NSManagedObject Subclass…"
+//  to keep your implementation up to date with your model.
+//
+
+import Foundation
+import CoreData
+
+extension DateTodoPoint {
+
+    @NSManaged var dateType: NSNumber?
+    @NSManaged var weekNumber: NSNumber?
+
+}
